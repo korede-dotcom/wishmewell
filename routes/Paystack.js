8af81de4313d4de4e48de5e5bcaa807f7f1a1f7f
corede <korede@tm30.net>
2024-12-12 21:12:40 +0100

    testing booking

--- routes/Paystack.js
+++ routes/Paystack.js
@@ -100,20 +100,23 @@ routes.post('/paystack/initialize/reception', checkAuthCookie,expressAsyncHandle
       category_id,
       status: "success",
       room_number,
-      [Op.or]: [
+      [Op.and]: [
         {
-          // Overlapping start time
-          start: {
-            [Op.lt]: Sequelize.literal(`TO_TIMESTAMP('${end} 12:00:00+01', 'YYYY-MM-DD HH24:MI:SS')`)
-          },
           end: {
             [Op.gt]: Sequelize.literal(`TO_TIMESTAMP('${start} 12:00:00+01', 'YYYY-MM-DD HH24:MI:SS')`)
+          }
+        },
+        {
+          start: {
+            [Op.lt]: Sequelize.literal(`TO_TIMESTAMP('${end} 12:00:00+01', 'YYYY-MM-DD HH24:MI:SS')`)
           }
         }
       ]
     }
   });
-  console.log("🚀 ~ routes.post ~ bookedRooms:", bookedRooms)
+  
+  console.log("🚀 ~ routes.post ~ bookedRooms:", bookedRooms);
+  
   
 
 // if (bookedRooms > 0) {
